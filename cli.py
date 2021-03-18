a1064f78999003b17bcbc3a5f433576d0f1e3f50
lukew3 <lukew25073@gmail.com>
2021-03-17 22:44:35 -0400

    Added cli with rename user, not working deploy script

---
+++ cli.py
@@ -0,0 +1,48 @@
+import click
+from pymongo import MongoClient
+import json
+import os
+
+client = MongoClient()
+db = client.buildmyidea
+
+with open('config.json') as config_file:
+	config = json.load(config_file)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('old_username')
+@click.argument('new_username')
+def renameuser(old_username, new_username):
+    """ Rename a user in the database, replace all instances of name """
+    db.user.update_one({"username": old_username}, { "$set": {"username": new_username} })
+    db.idea.update_many({"creator": old_username }, { "$set": { "creator": new_username } })
+    print("User " + old_username + " renamed to: " + new_username)
+
+@cli.command()
+def deploy():
+    """ Deploy to server """
+    password = config.get('SERVER_PASSWORD')
+    script = f"""
+      echo "Building app"
+      cd frontend
+      npm run build
+      echo "Deploying build files to server"
+      echo {password} | rsync -avPS build/ luke@lukew3.com:/home/luke/buildmyidea/build/
+      echo "Git pulling on server"
+      sshpass -p {password} ssh luke@lukew3.com << DONE
+      cd ~/buildmyidea
+      git pull
+      cd ..
+      echo {password} | sudo -S ./notes
+      DONE
+      echo "Deployment complete"
+    """
+    os.system(script)
+
+
+if __name__ == "__main__":
+    cli()
