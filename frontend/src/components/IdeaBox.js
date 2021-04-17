a53fd1ddbe23b90af34304baaa2235825374f048
lukew3 <lukew25073@gmail.com>
2021-04-17 19:00:07 -0400

    Added score and improved likeIdea method

--- frontend/src/components/IdeaBox.js
+++ frontend/src/components/IdeaBox.js
@@ -14,30 +14,42 @@ class IdeaBox extends Component {
       idea: props.idea,
       boxStyle: props.boxStyle, //can be full, normal, or condensed(just showing the title)
       liked: props.idea.liked,
-      disliked: props.idea.disliked
+      disliked: props.idea.disliked,
+      score: props.idea.liked-props.idea.disliked,
+      boost: 0
     }
     this.likeIdea = this.likeIdea.bind(this);
     this.dislikeIdea = this.dislikeIdea.bind(this);
   }
 
   likeIdea() {
-    axiosApiInstance.post(`/api/like_idea`, {"ideaId": this.state.idea._id}).then(
-      this.setState({ liked: true, disliked: false })
-    ).catch(error => {
-      console.log(error);
-      console.log("must be logged in to rate ideas");
-      window.location.href = "https://buildmyidea.tk/login";
-    });
+    if (this.state.liked === false) { //this prevents sending requests when the idea is already liked
+      axiosApiInstance.post(`/api/like_idea`, {"ideaId": this.state.idea._id}).then(() => {
+        if (this.state.disliked) {
+          this.setState({ score: this.state.score + 1});
+        }
+        this.setState({ liked: true, disliked: false, score: this.state.score + 1 })
+      }).catch(error => {
+        console.log(error);
+        console.log("must be logged in to rate ideas");
+        window.location.href = "https://buildmyidea.tk/login";
+      });
+    }
   }
 
   dislikeIdea() {
-    axiosApiInstance.post(`/api/dislike_idea`, {"ideaId": this.state.idea._id}).then(
-      this.setState({ disliked: true, liked: false })
-    ).catch(error => {
-      console.log(error);
-      console.log("must be logged in to rate ideas");
-      window.location.href = "https://buildmyidea.tk/login";
-    });
+    if (this.state.disliked === false) {
+      axiosApiInstance.post(`/api/dislike_idea`, {"ideaId": this.state.idea._id}).then(() => {
+        if (this.state.liked) {
+          this.setState({ score: this.state.score - 1});
+        }
+        this.setState({ disliked: true, liked: false, score: this.state.score - 1})
+      }).catch(error => {
+        console.log(error);
+        console.log("must be logged in to rate ideas");
+        window.location.href = "https://buildmyidea.tk/login";
+      });
+    }
   }
 
   render() {
@@ -53,8 +65,11 @@ class IdeaBox extends Component {
     return (
       <div id={idea._id} key={idea._id} className={`ideaBox ${this.state.boxStyle}`}>
         <div className="likeSection">
-          <i className={`fa fa-thumbs-up likeButton ratingButton ${likedClass}`} aria-hidden="true" onClick={this.likeIdea}></i>
-          <i className={`fa fa-thumbs-down dislikeButton ratingButton ${dislikedClass}`} aria-hidden="true" onClick={this.dislikeIdea}></i>
+          <div className="thumbs">
+            <i className={`fa fa-thumbs-up likeButton ratingButton ${likedClass}`} aria-hidden="true" onClick={this.likeIdea}></i>
+            <i className={`fa fa-thumbs-down dislikeButton ratingButton ${dislikedClass}`} aria-hidden="true" onClick={this.dislikeIdea}></i>
+          </div>
+          <p>Score: {this.state.score}</p>
         </div>
         <div className="ideaBoxContents">
           <div className="ideaBoxUpper">
