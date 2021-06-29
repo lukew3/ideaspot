b6bf12af3795184c525f6f4e19f531d0ee17b646
lukew3 <lukew25073@gmail.com>
2021-06-29 16:36:57 -0400

    added Comment component and updated styling, removed old comment handler

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -17,16 +17,10 @@ class Comments extends Component {
         <div className="comments normalBox">
           <NewComment ideaId={this.state.ideaId} parentId=""/>
           {(this.state.comments).map((comment, index) => (
-            <div className="comment" key={index}>
-              <Link to={`/${comment.user}`}>{comment.user}</Link>
-              <br/>
-              <div className="commentString">
-                <div className="commentStringLeft"></div>
-                <div className="commentStringRight">
-                  {comment.comment}
-                </div>
-              </div>
-            </div>
+            <Comment
+              ideaId={this.state.ideaId}
+              comment={comment}
+              parentId={""} />
           ))}
         </div>
       );
@@ -40,6 +34,37 @@ class Comments extends Component {
         </div>
       )
     }
+  }
+}
+
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideaId: props.ideaId,
+      comment: props.comment,
+      parentId: props.parentId,
+    }
+  }
+  render() {
+    return(
+      <div className="comment" key={this.state.comment.comment}> {//key should be comment._id
+      }
+        <Link to={`/${this.state.comment.user}`}>{this.state.comment.user}</Link>
+        <br/>
+        <div className="commentString">
+          <div className="commentStringLeft"></div>
+          <div className="commentStringRight">
+            {this.state.comment.comment}
+            <div className="commentActions">
+              <a href="#" onClick={() => {
+                return <p>Test</p>
+              }}>Reply</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -91,60 +116,5 @@ class NewComment extends Component {
     );
   }
 }
-
-/*
-class NewCommentOld extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      idea: props.idea,
-      commentContent: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({ [name]: value });
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.idea)
-    await axiosApiInstance.post(`/api/add_comment`,
-      { ideaId: this.state.idea._id,
-        commentContent: this.state.commentContent }
-    ).then(response => {
-      this.props.addCommentLocal(response.data.user, response.data.comment);
-      console.log('comment added')
-    }).catch(error => {
-      console.log(error)
-      console.log("Login invalid");
-    });
-  }
-
-  render() {
-    return(
-      <div className="standardBorder">
-        <form onSubmit={this.handleSubmit}>
-          <h3>Add a comment</h3>
-          <textarea
-            type="text"
-            name="commentContent"
-            className="createIdeaDescription"
-            placeholder="What do you think?..."
-            value={this.state.commentContent}
-            onChange={this.handleInputChange} />
-          <br/>
-          <input type="submit" value="Submit"/>
-        </form>
-      </div>
-    )
-  }
-}
-*/
 
 export default Comments;
