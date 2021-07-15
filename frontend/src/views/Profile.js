2ec7b324e7500d3168e5f635e0bdc8a7f3856e4c
lukew3 <lukew25073@gmail.com>
2021-07-14 21:06:14 -0400

    able to switch tabs on profile

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -3,15 +3,18 @@ import '../styles/Profile.css';
 import { Link } from 'react-router-dom';
 import Cookie from 'js-cookie';
 import axiosApiInstance from '../helper.js';
-
+import queryString from 'query-string';
 
 class Profile extends Component {
   constructor(props){
     super(props);
+    let tab = queryString.parse(this.props.location.search).tab;
     this.state = {
       user: {
-        "ideas": []
-      }
+        "ideas": [],
+      },
+      activeTab: tab === undefined ? "ideas" : tab,
+      builds: {},
     }
   }
 
@@ -20,12 +23,53 @@ class Profile extends Component {
       if (response.data.bio === undefined) response.data.bio = "";
       if (response.data.buildCount === undefined) response.data.buildCount = 0;
       this.setState({ user: response.data });
+    }).then(() => {
+      this.setActiveTab(this.state.activeTab);
     }).catch(error => {
       console.log(error);
     });
   }
 
+  setActiveTab(tabName, prevActiveTab=undefined) {
+    if (this.state.activeTab) {
+      document.getElementById(`${this.state.activeTab}Tab`).classList.remove("activeTab");
+    }
+    document.getElementById(`${tabName}Tab`).classList.add("activeTab");
+    this.setState({activeTab: tabName});
+    if (tabName !== "ideas") {
+      this.props.history.push(`/${this.props.match.params.username}?tab=${tabName}`)
+    } else {
+      this.props.history.push(`/${this.props.match.params.username}`)
+    }
+  }
+
   render() {
+    const renderTabContent = () => {
+      if (this.state.activeTab === "ideas") {
+        return (
+          <div className="ideaFeed">
+            {this.state.user.ideas.map((idea, index) => (
+              <div className="standardBorder profileIdea">
+                <Link to={`/idea/${idea._id}`}><h3>{idea.title}</h3></Link>
+              </div>
+            ))}
+          </div>
+        )
+      } else if (this.state.activeTab === "builds") {
+        return <div>Builds data</div>
+        /*
+        axiosApiInstance.get(`/api/user/${this.props.match.params.username}/builds`).then(response => {
+          this.setState({builds: response.data});
+        }).then(() => {
+          return (
+            <div>Test</div>
+          )
+        })
+        */
+      } else if (this.state.activeTab === "teams") {
+        return <div>Teams data</div>
+      }
+    }
     return (
       <div className="profilePage">
         <div className="profileGrid">
@@ -47,17 +91,15 @@ class Profile extends Component {
 
           <div className="profileRight standardBorder">
             <div className="profileSectionSelector">
-              <div className="profileSelectorItem selActive selIdeas">Ideas ({this.state.user.ideasCount})</div>
-              <div className="profileSelectorItem selBuilds">Builds ({this.state.user.buildCount})</div>
-              <div className="profileSelectorItem selTeams">Teams</div>
+              <div id="ideasTab" className="profileSelectorItem" onClick={() => {
+                this.setActiveTab("ideas");
+              }}>Ideas ({this.state.user.ideasCount})</div>
+              <div id="buildsTab" className="profileSelectorItem" onClick={() => {
+                this.setActiveTab("builds")
+              }}>Builds ({this.state.user.buildCount})</div>
+              <div id="teamsTab" className="profileSelectorItem" onClick={() => { this.setActiveTab("teams")}}>Teams</div>
             </div>
-            <div className="ideaFeed">
-              {this.state.user.ideas.map((idea, index) => (
-                <div className="standardBorder profileIdea">
-                  <Link to={`/idea/${idea._id}`}><h3>{idea.title}</h3></Link>
-                </div>
-              ))}
-            </div>
+            {renderTabContent()}
           </div>
         </div>
       </div>
