4123b8a157628d82a697beb5bc1caa43dd03e905
lukew3 <lukew25073@gmail.com>
2021-10-14 13:11:05 -0400

    updated ideaform submit button styling

--- frontend/src/components/IdeaForm.js
+++ frontend/src/components/IdeaForm.js
@@ -122,7 +122,7 @@ class IdeaForm extends Component {
           {
             //add up to 3 tags
           }
-          <input type="submit" value="Submit"/>
+          <input type="submit" className="ideaFormSubmit" value="Submit"/>
         </form>
       </div>
       </div>
