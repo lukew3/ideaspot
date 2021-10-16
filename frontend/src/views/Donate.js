734497dee6b6666f348ccd7fa9994bcf616d6a2e
lukew3 <lukew25073@gmail.com>
2021-10-16 12:53:13 -0400

    Added liberapay

--- frontend/src/views/Donate.js
+++ frontend/src/views/Donate.js
@@ -7,7 +7,9 @@ class Donate extends Component {
       <div className="donateMain">
         <h1>Donate</h1>
         <p>
-          You can donate to the project on venmo @lukew3. Just mention ideaspot and I'll make sure the funds go to the right place.
+          To make a recurring donation, go to <a href="https://liberapay.com/Ideaspot/">https://liberapay.com/Ideaspot/</a>.
+          If you only want to donate once, you can donate to the project on venmo @lukew3.
+          Just mention ideaspot and I'll make sure the funds go to the right place.
         </p>
       </div>
     );
