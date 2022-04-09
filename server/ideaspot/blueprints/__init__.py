5f8d2824a7f68f7a40811015fb287118bf39c710
lukew3 <lukew25073@gmail.com>
2022-04-09 19:31:31 -0400

    working at fastapi

--- server/ideaspot/blueprints/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import auth_bp
-from .comments import comments_bp
-from .idea import idea_bp
-from .list import list_bp
-from .user import user_bp
-from .voting import voting_bp
--- server/ideaspot/db.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient()
-db = client.ideaspot
